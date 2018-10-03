c20e85f32ffaec6a613020551f787340d1069c80
gitlabadmin <admingitlab@localhost>
2018-10-03 09:13:10 +0000

    edited

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var http = require('http');
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Ho World, Its good \n");
+  response.end("Hello World, Its good to be back\n");
 });
 
 // listen on localhost:8000
